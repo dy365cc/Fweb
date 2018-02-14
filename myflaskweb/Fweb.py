a31220fa6abeb0560240c386eed94f995f5de5c0
dy365cc <65941821@qq.com>
2018-02-14 16:52:42 +0800

    修改了主页和登录之后的面板！

--- myflaskweb/Fweb.py
+++ myflaskweb/Fweb.py
@@ -3,6 +3,7 @@ from flask import Flask,url_for,redirect,request,session
 from flask import render_template
 import Fform
 from mysql import MyDb
+import time
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 #Bootstrap(app)
@@ -17,33 +18,36 @@ def fb_index():
         v_pwd = logform.name.data
 
         #return redirect(url_for('login_do'))
-    return render_template('mypage/login.html')
+    return render_template('mypage/index.html')
 
 
 
 @app.route('/login_do',methods=['POST'])
 def login_do1():
     logform = Fform.loginfor_form()
-    p_name = request.form['name']
-    p_pwd = request.form['pwd']
+    p_name = request.form['username']
+    p_pwd = request.form['password']
     my_db = MyDb()
     my_data = []
     if p_name != "":
-        my_sql = "select * from user where username = '"+p_name+"';"
+        my_sql = "select * from user where p_name = '"+p_name+"';"
     else:
-        my_sql = "select * from user where password = '"+p_pwd+"';"
+        my_sql = "select * from user where p_pwd = '"+p_pwd+"';"
     my_db.getdata(my_sql)
     if len(my_db.rst) > 0:
         my_data = my_db.rst
     else:
-        return render_template('mypage/index.html',v_form = logform,v_info = "用户名或密码错！")
-    if my_data[0]["password"] == p_pwd:
+        return render_template('mypage/index.html', v_info = '用户名密码错误！')
+    if my_data[0]["p_pwd"] == p_pwd:
         session['uid'] = my_data[0]['iduser']
+        session['uname'] = my_data[0]['p_name']
+        session['ulevel'] = my_data[0]['p_level']
+        session['utime'] = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
         return render_template('mypage/loginpage.html',l_name = p_name,l_pwd = p_pwd)
     else:
-        return render_template('mypage/index.html',v_form = logform,v_info = "用户名或密码错！")
+        return render_template('mypage/index.html', v_info = '用户名密码错误！')
 
 
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0',port=81)
+    app.run(debug=False,host='0.0.0.0',port=8000)
